9ae5b5d38020588f35ece003a3686c669731dc03
gms <gms@paykhan.orn>
2020-05-15 19:02:25 +0900

    @Entity Posts클래스까지 생성 (3장 JPA)

---
+++ src/main/java/su/duniv3rse/jojoldu/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package su.duniv3rse.jojoldu.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor        // 선언된 모든 final필드가 포함된 생성자를 생성 (final이 없는 필드는 생성자에 포함 x)
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
